2868d3022bfc6b8b1984be100bc571da9c61cc2a
pluto-wang <pluto@gmail.com>
2017-01-07 20:45:45 +0800

    associate to web

--- src/ir.py
+++ src/ir.py
@@ -1,7 +1,10 @@
 from twisted.internet import reactor
 from udpwkpf import WuClass, Device
-import sys
+import sys,time
 from udpwkpf_io_interface import *
+import requests
+import json
+myurl="http://10.1.2.23:3000/ir"
 
 Pin1 = 2 # depends on which analog port
 Pin2 = 5
@@ -14,38 +17,43 @@ class Ir(WuClass):
         self.ir2_sensor_aio = pin_mode(Pin2, PIN_TYPE_ANALOG)
         self.temp1 = 0
         self.temp2 = 0
-        self.count = 0
+        self.count = 8
         print "Ir sensor init!"
 
     def update(self,obj,pID=None,val=None):
+        time.sleep(0.5)
         try:
             ir1 = analog_read(self.ir1_sensor_aio)
             obj.setProperty(0, ir1) 
             ir2 = analog_read(self.ir2_sensor_aio)
             obj.setProperty(0, ir2)
-            #print "Sound sensor analog pin: %d, value: %d" % (Pin2, ir2)
- 
+            print "Sound sensor analog pin: %d, value: %d" % (Pin1, ir1)
+            print "Sound sensor analog pin: %d, value: %d" % (Pin2, ir2)
+
             if ir1 > 250:
                 self.temp1 = ir1
             elif ir2 > 250:
                 self.temp2 = ir2
 
-            if self.temp1 > 250 or self.temp2 > 25:
+            if self.temp1 > 250 and self.temp2 > 250:
                 if self.temp1 > self.temp2:
                     print "get on"
-                    self.count = self.count + 1
+                    if self.count > 0: 
+                        self.count = self.count - 1
                 elif self.temp1 < self.temp2:
                     print "get off"
-                    if self.count > 0:
-                        self.count = self.count-1 
+                    if self.count < 8:
+                        self.count = self.count+1 
                 self.temp1 = 0
                 self.temp2 = 0
             else:
                 print "nobody"
-            print "number of people=%d" %(self.count)    
+                print "number of people=%d" %(self.count)    
         except IOError:
             print ("Error")
-            
+
+        people={"count":self.count}                        
+        response = requests.post(myurl,json=people)
 
 class MyDevice(Device):
     def __init__(self,addr,localaddr):
