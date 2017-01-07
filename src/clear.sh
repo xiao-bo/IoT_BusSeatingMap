c1257e1aaf43f08bb350a29c3668734b37b4df44
xiao-bo <ay852852@gmail.com>
2017-01-07 11:38:03 +0800

    wukong clear script

---
+++ src/clear.sh
@@ -0,0 +1,12 @@
+rm master/.devices.pkl
+rm master/*.pyc 
+rm master/*.json 
+rm master/change*
+rm master/static/js/__comp__*
+
+rm gateway/.devices.pkl
+rm gateway/*.pyc 
+rm gateway/*.json 
+rm gateway/device*
+
+rm gateway/udpwkpf/*.pyc *.json
